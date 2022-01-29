7282a70a9238b2dfc0a94f4f5577d89fd6484539
Marcin <Madzik890@hotmail.com>
2022-01-29 06:29:57 +0100

    small fix

--- src/threads/threads.h
+++ src/threads/threads.h
@@ -1,8 +1,6 @@
 #ifndef THREADS_H
 #define THREADS_H
 
-#define THREADS_SIZE 5
-
 #define START_THREAD(func)                                  \
         if(CreateThread(func) != 0)                         \
         {  printf("Cannot create thread!"); return -1; }    \
